775fa99f9ab9bba2fb648019fe61250d039a4f4b
Elahe Irannezhad <19331262+Elahe-Irannezhad@users.noreply.github.com>
2024-03-19 18:13:46 +0330

    Update views.py

--- calculator_project/calculator/views.py
+++ calculator_project/calculator/views.py
@@ -2,36 +2,37 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_GET
 from django.contrib.auth.decorators import login_required
 from .models import UserAction
+from django.shortcuts import render
 
 @login_required
 def calculate(request):
 
-    n1 = int(request.GET.get('n1'))
-    n2 = int(request.GET.get('n2'))
-    op = request.GET.get('op')
+    if request.method == 'POST':
+        
+        n1 = int(request.POST.get('n1', ''))
+        n2 = int(request.POST.get('n2', ''))
+        op = request.POST.get('op', '')
 
-    if op == "add":
-        result = n1 + n2
-        operator = "+"
+        if op == "add":
+            result = n1 + n2
+            operator = "+"
 
-    elif op == "subtract":
-        result = n1 - n2
-        operator = "-"
+        elif op == "subtract":
+            result = n1 - n2
+            operator = "-"
 
-    elif op == "multiply":
-        result = n1 * n2
-        operator = "*"
+        elif op == "multiply":
+            result = n1 * n2
+            operator = "*"
 
-    elif op == "divide":
-        if n2 == 0:
-            return HttpResponse("Division by Zero Is Not Allowed.")
-        result = n1 / n2
-        operator = "/"
+        elif op == "divide":
+            if n2 == 0:
+                return HttpResponse("Division by Zero Is Not Allowed.")
+            result = n1 / n2
+            operator = "/"
 
-    else:
-        return HttpResponse("Invalid Operator.")
-    
-    actionRecord = f"User Performed {op}."
-    userAction = UserAction.objects.create(user = request.user, action = actionRecord)
+        actionRecord = f"User Performed {op}."
+        userAction = UserAction.objects.create(user = request.user, action = actionRecord)
+        return render(request, 'calculator/result.html', {'result': result})
 
-    return HttpResponse(f'{n1} {operator} {n2} = {result}')
+    return render(request, 'calculator/calculate.html')
